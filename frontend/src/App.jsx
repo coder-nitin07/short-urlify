8bd0d7c6189d0ab1b9a8acd019bb1278b40905d4
coder-nitin07 <nitinthakur87504@gmail.com>
2025-08-06 22:53:02 +0530

    Update app.jsx file

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -15,7 +15,7 @@ function App(){
         <Navbar />
 
             <Routes>
-                <Route path="/" element={<Navigate to="/home" />} />
+                <Route path="/" element={<Navigate to="/login" />} />
                 <Route path='/signup' element={<Signup />} />
                 <Route path='/login' element={<Login />} />
                 <Route path='/home' element={<Home />} />
