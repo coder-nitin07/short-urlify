2cf1279d11a6c87fb7cb80ff03d209d4e0ba1a4c
coder-nitin07 <nitinthakur87504@gmail.com>
2025-08-06 15:18:13 +0530

    Setup Frontend

---
+++ frontend/src/App.jsx
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <div className="min-h-screen flex items-center justify-center bg-white dark:bg-gray-900 text-black dark:text-white">
+      <h1 className="text-3xl font-bold">Shorturilify</h1>
+    </div>
+  );
+}
+
+export default App;
