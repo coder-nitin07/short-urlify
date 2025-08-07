68b199bd9d31d988f79e6f906c435e10d3e0c90f
coder-nitin07 <nitinthakur87504@gmail.com>
2025-08-07 10:49:10 +0530

    tab name chnages

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -4,7 +4,6 @@ import Login from './pages/Login';
 import Home from './pages/Home';
 import AllUrls from './pages/AllUrls';
 import Navbar from './components/Navbar';
-import { ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 import Footer from './components/Footer';
 
